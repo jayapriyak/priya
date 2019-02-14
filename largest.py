bd99670bd8280c3534444b8d51b62e22d613195f
jayapriyak <47291244+jayapriyak@users.noreply.github.com>
2019-02-14 00:28:39 -0800

    Update largest.py

--- largest.py
+++ largest.py
@@ -1,7 +1,7 @@
-a1,a2,a3=list(map(int,input().split()))
-if a1>a2 and a1>a3:
-	print(a1)
-elif a2>a3:
-	print(a2)
+t1,t2,t3=list(map(int,input().split()))
+if t1>t2 and t1>t3:
+	print(t1)
+elif t2>t3:
+	print(t2)
 else:
-	print(a3)
+	print(t3)
