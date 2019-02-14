f267fd311e2f5dfef66ae14dda70ea62fb4d75eb
jayapriyak <47291244+jayapriyak@users.noreply.github.com>
2019-02-14 00:12:28 -0800

    Update vowel r con.py

--- vowel r con.py
+++ vowel r con.py
@@ -1,6 +1,10 @@
-za=input()
-if za=="a" or za=="e" or za=="i" or za=="o" or za=="u":
-	print("Vowel")
+ba=input()
+if ba.isalpha():
+	za=ba.lower()
+	if za=="a" or za=="e" or za=="i" or za=="o" or za=="u":
+		print("Vowel")
+	else:
+		print("Consonant")
 else:
-	print("Consonant")
+	print("invalid")
 	
